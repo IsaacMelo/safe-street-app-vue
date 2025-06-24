a69541af87f1d101b5c7e99c4e11ff6169b80e45
João Paulo <joao.silva3371@etec.sp.gov.br>
2025-06-23 21:05:11 -0300

    initial commit

---
+++ nuxt.config.ts
@@ -0,0 +1,19 @@
+// https://nuxt.com/docs/api/configuration/nuxt-config
+export default defineNuxtConfig({
+  compatibilityDate: '2025-05-15',
+  devtools: { enabled: true },
+
+  nitro: {
+    preset: 'static' // importante para gerar os arquivos no `dist`
+  },
+  modules: [
+    '@nuxt/content',
+    '@nuxt/eslint',
+    '@nuxt/fonts',
+    '@nuxt/icon',
+    '@nuxt/image',
+    '@nuxt/scripts',
+    '@nuxt/test-utils',
+    '@nuxt/ui'
+  ]
+})
